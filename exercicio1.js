bdf0479060c30f5a85a9167f272fd7f636253785
fabskamo <fabiana.kamo@hotmail.com>
2021-11-28 15:13:16 -0300

    web espacial

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,16 @@
 // Faça o exercício da equação de GRAVITAÇÃO UNIVERSAL aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+let botaoCalcular = document.querySelector('#calcular');
+let inputResultado = document.querySelector('#resultado');
+
+let inputMassa1 = document.querySelector('#massa1');
+let inputMassa2 = document.querySelector('#massa2');
+let inputDist = document.querySelector('#distancia');
+let inputCte = document.querySelector('#constante');
+
+botaoCalcular.addEventListener('click', function() {
+
+    let resultado = ((inputCte.value * inputMassa1.value * inputMassa2.value) / inputDist.value ** 2);
+    inputResultado.value = resultado;
+});
--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,21 @@
 // Faça o exercício dos PARÁGRAFOS aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+let botaoExpandirRetrair = document.querySelectorAll('button');
+
+for (let b of botaoExpandirRetrair) {
+
+    b.addEventListener('click', function() {
+        if (b.innerHTML == '+') {
+            b.innerHTML = '-';
+            let pai = b.parentNode;
+            pai.classList.add('expandido');
+        } else {
+            if (b.innerHTML == '-') {
+                b.innerHTML = '+';
+                let pai = b.parentNode;
+                pai.classList.remove('expandido');
+            }
+        }
+    });
+}
